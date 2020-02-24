05e38ffc0c2e7b921630bcf83ab03e5dce2095e7
Tao Zhang <zta@outlook.com>
2020-02-24 20:25:54 +0800

    init

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+using std::cout;
+using std::endl;
+using std::string;
+
+int main(int argc, char* argv[]) {
+
+  cout << "hello, little player." << endl;
+
+
+  return 0;
+}
